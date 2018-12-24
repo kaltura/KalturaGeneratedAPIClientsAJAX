a206189f68cdb513cb02b92aaf7de28e3c7b2c17
Kaltura CSI <community@kaltura.com>
2018-12-24 08:20:21 -0500

    New clientlib ver

--- test/config.js
+++ test/config.js
@@ -1,4 +1,4 @@
 
-var secret = "83c3d4449a3abbe92ca5966f5f010eb4";
-var partnerId = 1931861;
+var secret = "@YOUR_ADMIN_SECRET@";
+var partnerId = @YOUR_PARTNER_ID@;
 var serviceUrl = "https://www.kaltura.com";
