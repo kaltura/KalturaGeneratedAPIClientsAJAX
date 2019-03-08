d8c2d77be32dcc09c0db50b3871487513d26da1f
Kaltura CSI <community@kaltura.com>
2019-03-08 02:54:42 -0500

    New clientlib ver

--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -6391,59 +6391,6 @@ var KalturaCaptionAssetItemService = {
 }
 
 /**
- *Class definition for the Kaltura service: confMaps.
- **/
-var KalturaConfMapsService = {
-	/**
-	 * Add configuration map.
-	 * @param	map	KalturaConfMaps		 (optional)
-	 **/
-	add: function(map){
-		var kparams = new Object();
-		kparams.map = map;
-		return new KalturaRequestBuilder("confmaps_confmaps", "add", kparams);
-	},
-	
-	/**
-	 * Get configuration map.
-	 * @param	filter	KalturaConfMapsFilter		 (optional)
-	 **/
-	get: function(filter){
-		var kparams = new Object();
-		kparams.filter = filter;
-		return new KalturaRequestBuilder("confmaps_confmaps", "get", kparams);
-	},
-	
-	/**
-	 * List configuration maps names.
-	 **/
-	getMapNames: function(){
-		var kparams = new Object();
-		return new KalturaRequestBuilder("confmaps_confmaps", "getMapNames", kparams);
-	},
-	
-	/**
-	 * List configuration maps.
-	 * @param	filter	KalturaConfMapsFilter		 (optional)
-	 **/
-	listAction: function(filter){
-		var kparams = new Object();
-		kparams.filter = filter;
-		return new KalturaRequestBuilder("confmaps_confmaps", "list", kparams);
-	},
-	
-	/**
-	 * Update configuration map.
-	 * @param	map	KalturaConfMaps		 (optional)
-	 **/
-	update: function(map){
-		var kparams = new Object();
-		kparams.map = map;
-		return new KalturaRequestBuilder("confmaps_confmaps", "update", kparams);
-	}
-}
-
-/**
  *Class definition for the Kaltura service: distributionProfile.
  **/
 var KalturaDistributionProfileService = {
@@ -10082,7 +10029,7 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:19-03-07');
+	this.setClientTag('ajax:19-03-08');
 	this.setApiVersion('14.16.0');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
