245992a20ed452f7dd6cc683c8e1281b61a72533
Kaltura CSI <community@kaltura.com>
2019-07-24 01:36:08 -0400

    New clientlib ver

--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -10200,7 +10200,7 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:19-07-23');
+	this.setClientTag('ajax:19-07-24');
 	this.setApiVersion('15.4.0');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
