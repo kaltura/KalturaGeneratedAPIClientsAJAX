33239fef45010ebafc6dbfab1f9d88a64c44bd72
Kaltura CSI <community@kaltura.com>
2023-03-23 14:05:22 -0400

    New clientlib ver

--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -10394,6 +10394,71 @@ var KalturaVirusScanProfileService = {
 }
 
 /**
+ *Class definition for the Kaltura service: virtualEvent.
+ **/
+var KalturaVirtualEventService = {
+	/**
+	 * Add a new virtual event.
+	 * @param	virtualEvent	KalturaVirtualEvent		 (optional)
+	 **/
+	add: function(virtualEvent){
+		var kparams = new Object();
+		kparams.virtualEvent = virtualEvent;
+		return new KalturaRequestBuilder("virtualevent_virtualevent", "add", kparams);
+	},
+	
+	/**
+	 * Delete a virtual event.
+	 * @param	id	int		 (optional)
+	 **/
+	deleteAction: function(id){
+		var kparams = new Object();
+		kparams.id = id;
+		return new KalturaRequestBuilder("virtualevent_virtualevent", "delete", kparams);
+	},
+	
+	/**
+	 * Retrieve a virtual event by id.
+	 * @param	id	int		 (optional)
+	 **/
+	get: function(id){
+		var kparams = new Object();
+		kparams.id = id;
+		return new KalturaRequestBuilder("virtualevent_virtualevent", "get", kparams);
+	},
+	
+	/**
+	 * List virtual events.
+	 * @param	filter	KalturaVirtualEventFilter		 (optional, default: null)
+	 * @param	pager	KalturaFilterPager		 (optional, default: null)
+	 **/
+	listAction: function(filter, pager){
+		if(!filter)
+			filter = null;
+		if(!pager)
+			pager = null;
+		var kparams = new Object();
+		if (filter != null)
+			kparams.filter = filter;
+		if (pager != null)
+			kparams.pager = pager;
+		return new KalturaRequestBuilder("virtualevent_virtualevent", "list", kparams);
+	},
+	
+	/**
+	 * Update an existing virtual event.
+	 * @param	id	int		 (optional)
+	 * @param	virtualEvent	KalturaVirtualEvent		 (optional)
+	 **/
+	update: function(id, virtualEvent){
+		var kparams = new Object();
+		kparams.id = id;
+		kparams.virtualEvent = virtualEvent;
+		return new KalturaRequestBuilder("virtualevent_virtualevent", "update", kparams);
+	}
+}
+
+/**
  *Class definition for the Kaltura service: widevineDrm.
  **/
 var KalturaWidevineDrmService = {
